ca2c18a68b44328bda15dc541e5a0c8d0d4c14bd
Hunter <no@no.com>
2021-08-01 11:35:19 -0400

    add category

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ let transporter = nodemailer.createTransport({
 
 // Step 2
 let mailOptions = {
-  from: 'anhcu714@gmail.com', // TODO: email sender
-  to: 'anhcu@hotmail.com', // TODO: email receiver
+  from: 'toolin.around21@gmail.com', // TODO: email sender
+  to: 'toolin.around21@gmail.com', // TODO: email receiver
   subject: 'Nodemailer - Test',
-  text: 'Wooohooo it works!!'
+  text: 'Wooohooo'
 };
 
 // Step 3
